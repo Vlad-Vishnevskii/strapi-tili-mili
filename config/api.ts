7faa81ec32eb915dca9111796c58fec72891fd5a
unknown <vishnevskiyvv1@sibintek.ru>
2026-05-31 22:44:33 +0300

    fix api limit

--- config/api.ts
+++ config/api.ts
@@ -2,8 +2,8 @@ import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Api = {
   rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
+    defaultLimit: 1000,
+    maxLimit: 1000,
     withCount: true,
   },
 };
